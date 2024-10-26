2d0a9e0bff56c4be4a4889e955d3fbe9d4ebfb1c
Dmytro Reviakin <thefaust31@gmail.com>
2024-10-26 15:07:17 +0300

    fix cors

--- src/server.js
+++ src/server.js
@@ -2,48 +2,57 @@ import express from 'express';
 // import path from 'node:path';
 import cookieParser from 'cookie-parser';
 import pino from 'pino-http';
+import cors from 'cors';
 import characterRoutes from './routes/characterRoutes.js';
 import { AVATAR_DIR, IMAGE_DIR } from './constants/index.js';
 // import { UPLOAD_DIR } from './constants/index.js';
 
 const PORT = process.env.PORT || 3000;
 
-
 export const setupServer = () => {
-const app = express();
+  const app = express();
 
-app.use(
-  express.json({
-    type: ['application/json', 'application/vnd.api+json'],
-    limit: '100kb'
-  })
-);
+  app.use(
+    express.json({
+      type: ['application/json', 'application/vnd.api+json'],
+      limit: '100kb',
+    }),
+  );
 
-app.use(cookieParser());
+  app.use(cookieParser());
 
-app.use(
-pino({
-    transport: {
-        target: 'pino-pretty'
-    }
-})
-);
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
 
+  const allowedOrigins = [
+    'http://localhost:5173',
+    'https://super-hero-front.vercel.app',
+  ];
 
-// app.use(express.static(path.resolve('src', 'uploads')));
+  const corsOptions = {
+    origin: allowedOrigins,
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'], // разрешенные методы
+    credentials: true, // если вы используете куки
+  };
 
-app.use('/characters', characterRoutes);
-// app.use('/avatars', express.static(path.join(UPLOAD_DIR, 'avatars')));
-app.use('/avatars', express.static(AVATAR_DIR));
-app.use('/images', express.static(IMAGE_DIR));
+  app.use(cors(corsOptions));
 
+  // app.use(express.static(path.resolve('src', 'uploads')));
 
-// app.use('/character/avatar', express.static(UPLOAD_DIR));
-// app.use('/character/image', express.static(UPLOAD_DIR));
+  app.use('/characters', characterRoutes);
+  // app.use('/avatars', express.static(path.join(UPLOAD_DIR, 'avatars')));
+  app.use('/avatars', express.static(AVATAR_DIR));
+  app.use('/images', express.static(IMAGE_DIR));
 
+  // app.use('/character/avatar', express.static(UPLOAD_DIR));
+  // app.use('/character/image', express.static(UPLOAD_DIR));
 
-app.listen(PORT, () => {
+  app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-});
+  });
 };
-
